9d5e56175c472d15eef1dde5a199da7efc2f9d7b
Ieva Šinke <ieva.sinke@gmail.com>
2024-12-18 10:24:41 +0200

    login request, register

--- app/Traits/ApiResponses.php
+++ app/Traits/ApiResponses.php
@@ -2,12 +2,15 @@
 
 namespace App\Traits;
 
-trait ApiResponses {
-	protected function ok($message) {
+trait ApiResponses
+{
+	protected function ok($message)
+	{
 		return $this->success($message, 200);
 	}
 
-	protected function success($message, $statusCode = 200) {
+	protected function success($message, $statusCode = 200)
+	{
 		return response()->json([
 			'message' => $message,
 			'status' => $statusCode
--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
 
 Route::get('/user', function (Request $request) {
-    return $request->user();
+	return $request->user();
 })->middleware('auth:sanctum');
