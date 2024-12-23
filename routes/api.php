159b1e5320a6b63bed2650f75b0e59049b3e8798
Ieva Šinke <ieva.sinke@gmail.com>
2024-12-23 15:36:34 +0200

    Auth and tokens

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
-use App\Http\Controllers\AuthController;
-use App\Models\Ticket;
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,10 +9,8 @@ use Illuminate\Support\Facades\Route;
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 
-Route::get('tickets', function() {
-	return Ticket::all();
-});
-
 Route::get('/user', function (Request $request) {
 	return $request->user();
 })->middleware('auth:sanctum');
+
+Route::prefix('v1')->group(base_path('routes/api_v1.php'));
