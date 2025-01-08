0772ab583d2895b7e1a635a40b0da36088ae8266
Ieva Šinke <ieva.sinke@gmail.com>
2025-01-08 15:23:26 +0200

    PUT/replace tickets author/tickets

--- app/Http/Controllers/Api/V1/AuthorTicketsController.php
+++ app/Http/Controllers/Api/V1/AuthorTicketsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Api\V1\ApiController;
 use App\Http\Filters\V1\TicketFilter;
 use App\Http\Resources\V1\TicketResource;
+use App\Http\Requests\Api\V1\ReplaceTicketRequest;
 use App\Models\Ticket;
 use App\Http\Requests\Api\V1\StoreTicketRequest;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
@@ -19,7 +20,33 @@ class AuthorTicketsController extends ApiController
 				->paginate()
 		);
 	}
+/**
+	 * Replace the specified resource in storage.
+	 */
+	public function replace(ReplaceTicketRequest $request, $authorId, $ticketId)
+	{
+		// PUT
+		try {
+			$ticket = Ticket::findOrFail($ticketId);
 
+			if ($ticket->user_id == $authorId) {
+				$model = [
+					'title' => $request->input('data.attributes.title'),
+					'description' => $request->input('data.attributes.description'),
+					'status' => $request->input('data.attributes.status'),
+					'user_id' => $request->input('data.relationships.author.data.id'),
+				];
+	
+				$ticket->update($model);
+	
+				return new TicketResource($ticket);
+			}
+// TODO: ticket doesn't belong to user
+
+		} catch (ModelNotFoundException $exception) {
+			return $this->error('Ticket cannot be found', 404);
+		}
+	}
 	/**
 	 * Store a newly created resource in storage.
 	 */
